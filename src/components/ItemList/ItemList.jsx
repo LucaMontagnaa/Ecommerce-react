afc360c0eb034dcdb88845b657d0237ee2a91cef
LucaMontagnaa <lucamontagna06@gmail.com>
2025-10-03 18:06:35 -0300

    id

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -5,7 +5,7 @@ import "./ItemList.css"
 const ItemList = ({productos}) => {
   return (
     <div className='contenedor-productos'>
-        {productos.map(item => <Item {...item}  />  )}
+        {productos.map(item => <Item key={item.id} {...item}  />  )}
     </div>
   )
 }
